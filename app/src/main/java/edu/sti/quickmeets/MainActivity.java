cee082c01676299acb8898fec2fead039cc5878f
BuildTools <unconfigured@null.spigotmc.org>
2018-11-04 18:37:21 +0800

    Initial commit

---
+++ app/src/main/java/edu/sti/quickmeets/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.sti.quickmeets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
